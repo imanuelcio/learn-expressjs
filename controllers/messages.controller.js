9beeb4959f9a693f0788fc6301583455bb2c8f87
imanuelcio <ciociocio009@gmail.com>
2024-02-06 18:20:53 +0700

    improvement: addd handle bars by installing hbs, and create layout and messages file

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, "..", "public", "images", "nekoneko2.png"));
+  // res.sendFile(path.join(__dirname, "..", "public", "images", "nekoneko2.png"));
+  res.render("messages", {
+    title: "Messages title",
+    friend: "RAUULLLLL ",
+  });
 }
 
 function postMessages(req, res) {
